ed70c9d1a874bacc30f43cc5075849566011ddbf
Lasta Apps <lastaappsdev@gmail.com>
2022-06-03 20:30:10 +0200

    feat: Theme, icon and splashscreen added

--- app/src/main/kotlin/cz/lastaapps/bakalari/MainActivity.kt
+++ app/src/main/kotlin/cz/lastaapps/bakalari/MainActivity.kt
@@ -3,20 +3,28 @@ package cz.lastaapps.bakalari
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cz.lastaapps.bakalari.ui.theme.BakalariTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import cz.lastaapps.bakalari.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().setKeepOnScreenCondition { false }
+
         setContent {
-            BakalariTheme {
+            AppTheme {
+                Surface(Modifier.fillMaxSize()) {
+                    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("Hello, Bakaláři 2.0")
+                    }
+                }
             }
         }
     }
