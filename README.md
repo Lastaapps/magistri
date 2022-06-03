6fafec91236f7a8b1b433b776c70392442ab4300
Lasta Apps <lastaappsdev@gmail.com>
2022-06-03 18:51:45 +0200

    Initial Bakaláři commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bakaláři client
+
+This app serves as an alternative client for Bakaláři project.
---
+++ app/src/main/kotlin/cz/lastaapps/bakalari/MainActivity.kt
@@ -0,0 +1,23 @@
+package cz.lastaapps.bakalari
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cz.lastaapps.bakalari.ui.theme.BakalariTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BakalariTheme {
+            }
+        }
+    }
+}
